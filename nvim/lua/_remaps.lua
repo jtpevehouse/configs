8ffa6a3ced2e789d3bbf76c6cd09bcff8166ad20
Josh Pevehouse <joshua.pevehouse@garmin.com>
2023-09-18 08:42:04 -0400

    Nvim: add function to set filetype to ansible
    Change-Id: I81b30228e5f419837f343aca0ada72a3d4f68210

--- nvim/lua/_remaps.lua
+++ nvim/lua/_remaps.lua
@@ -1,12 +1,17 @@
+-- FUNCTIONS
+-- SET FILETYPE AS ANSIBLE AND RESTART LSP
+vim.api.nvim_create_user_command("Setansible", function()
+	vim.cmd("setfiletype yaml.ansible")
+	vim.cmd("LspRestart")
+end, {})
+
 -- REMAPS
 local map = vim.api.nvim_set_keymap
 map("n", "<f1>", ":noh<cr>", { noremap = true })
 map("n", "<f2>", ":lua vim.lsp.buf.format()<cr>", { noremap = true })
 map("n", "<f5>", ":NvimTreeToggle<cr>", { noremap = true })
+map("n", "<f6>", ":Setansible<cr>", { noremap = true })
 map("n", "<f9>", ":horizontal resize -5<cr>", { noremap = true })
 map("n", "<f10>", ":horizontal resize +5<cr>", { noremap = true })
 map("n", "<f11>", ":vertical resize -5<cr>", { noremap = true })
 map("n", "<f12>", ":vertical resize +5<cr>", { noremap = true })
-
-vim.cmd([[ command! Q :qa ]])
-vim.cmd([[ command! WQ :wqa ]])
