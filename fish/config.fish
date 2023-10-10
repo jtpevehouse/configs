9ec603994eaf1c52232e6bc98dd6dae5f689f288
Josh Pevehouse <joshua.pevehouse@garmin.com>
2023-10-10 12:58:17 -0400

    FISH: add NNN commands
    Change-Id: I115ef8cc006cfd418ecaac38c8090226ea4c84a5

--- fish/config.fish
+++ fish/config.fish
@@ -3,16 +3,36 @@ theme_gruvbox dark medium
 
 alias 'nv'='nvim'
 alias 'll'='ls -lha'
+alias 'n'='nnn -C'
 alias 'vsc'='code .'
 alias 'pc'='podman-compose'
 
 set -gx EDITOR nvim
 
+function nnn --wraps nnn --description 'support nnn quit and change directory'
+    # Block nesting of nnn in subshells
+    if test -n "$NNNLVL" -a "$NNNLVL" -ge 1
+        echo "nnn is already running"
+        return
+    end
+
+    if test -n "$XDG_CONFIG_HOME"
+        set -x NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
+    else
+        set -x NNN_TMPFILE "$HOME/.config/nnn/.lastd"
+    end
+
+    command nnn $argv
+
+    if test -e $NNN_TMPFILE
+        source $NNN_TMPFILE
+        rm $NNN_TMPFILE
+    end
+end
+
 # LAUNCH TMUX BY DEFAULT
-if status is-interactive
-and not set -q TMUX
-	exec tmux a || \
-	tmux new -d -s home -n def && \
-	tmux new-window -n dev && \
-	tmux a -t "home"
+if not set -q TMUX
+    set -g TMUX tmux new-session -d -s home
+    eval $TMUX
+    tmux attach-session -d -t home 
 end
