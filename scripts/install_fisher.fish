2ca472f72d6850be628c8f7b3f2f9d9f13d96fee
jtpevehouse <josh.pevehouse@gmail.com>
2023-10-10 21:26:40 -0400

    FISH: install latest node version by default

--- scripts/install_fisher.fish
+++ scripts/install_fisher.fish
@@ -3,6 +3,7 @@
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source 
 fisher install jorgebucaran/fisher
 fisher install jorgebucaran/nvm.fish
+nvm install latest
 fisher install PatrickF1/fzf.fish
 fisher install jomik/fish-gruvbox
 fisher install catppuccin/fish
