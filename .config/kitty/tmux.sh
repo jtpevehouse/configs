49e75c79b3a0ca457e5ed2bd59701a37a8e4055a
Josh Pevehouse <joshua.pevehouse@garmin.com>
2026-02-04 11:44:08 -0500

    kitty: remove unused color schemes and update tmux init
    Change-Id: I03d2d0ed50523e718669d2ac61608a4d0d822f5c

--- .config/kitty/tmux.sh
+++ .config/kitty/tmux.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 TMUX_SESSION_NAME="home"
-# WORK CONFIG
-# MRN_DIR="/home/pevehousejosh/grmn/prj/mrn"
 
-# tmux new-session -A -d -s home -n def && \
-    #     tmux new-window -t $TMUX_SESSION_NAME -n "hydrogen" -c "$MRN_DIR/mfd-app" && \
-    #     tmux new-window -t $TMUX_SESSION_NAME -n "alchemy" -c "$MRN_DIR/alchemy" && \
-    #     tmux new-window -t $TMUX_SESSION_NAME -n "mrn-metrics" -c "$MRN_DIR/marine-metrics"
-
-# HOME CONFIG
-tmux new-session -A -d -s home -n home && \
-    tmux new-window -t $TMUX_SESSION_NAME -n "workspace" -c "~/workspace"
+tmux new-session -A -d -s home -n home &&
+	tmux new-window -t $TMUX_SESSION_NAME -n "workspace" -c "~/workspace"
 
 tmux attach -t home:def
