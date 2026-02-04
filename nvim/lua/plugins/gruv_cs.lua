5c198ed67bc06485733b5766237794154f9a25a1
Josh Pevehouse <joshua.pevehouse@garmin.com>
2026-02-04 07:48:52 -0500

    Nvim: update gruv options
    Removes the use of italics for strings and comments. Inverts selection and search results colors
    
    Change-Id: Ib5fd461abc4d8ba075d7aa3ae6a8bf2d3c223415

--- nvim/lua/plugins/gruv_cs.lua
+++ nvim/lua/plugins/gruv_cs.lua
@@ -9,18 +9,18 @@ return {
 	-- 		underline = true,
 	-- 		bold = true,
 	-- 		italic = {
-	-- 			strings = true,
-	-- 			comments = true,
+	-- 			strings = false,
+	-- 			comments = false,
 	-- 			operators = false,
 	-- 			folds = true,
 	-- 		},
 	-- 		strikethrough = true,
-	-- 		invert_selection = false,
+	-- 		invert_selection = true,
 	-- 		invert_signs = false,
 	-- 		invert_tabline = false,
 	-- 		invert_intend_guides = false,
-	-- 		inverse = false, -- invert background for search, diffs, statuslines and errors
-	-- 		contrast = "", -- can be "hard", "soft" or empty string
+	-- 		inverse = true, -- invert background for search, diffs, statuslines and errors
+	-- 		contrast = "soft", -- can be "hard", "soft" or empty string
 	-- 		palette_overrides = {},
 	-- 		overrides = {},
 	-- 		dim_inactive = false,
