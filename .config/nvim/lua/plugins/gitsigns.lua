b07bb0e34a51aab36926a8d2aa902057c798fc8c
jtpevehouse <josh.pevehouse@gmail.com>
2026-03-11 12:11:45 -0400

    nvim: change git blame line info
    Change-Id: I91897dc767b27bd61151ba41406f49709f1f30fd

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -26,7 +26,7 @@ return {
 				delay = 1000,
 				ignore_whitespace = false,
 			},
-			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d>",
+			current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
 			sign_priority = 6,
 			update_debounce = 100,
 			status_formatter = nil, -- Use default
