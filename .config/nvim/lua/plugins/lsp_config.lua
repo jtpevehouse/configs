fcbc98297c355c79d356b5124a11e0139b88b16b
jtpevehouse <josh.pevehouse@gmail.com>
2026-03-17 20:59:01 -0400

    Nvim: swap pylsp for ty

--- .config/nvim/lua/plugins/lsp_config.lua
+++ .config/nvim/lua/plugins/lsp_config.lua
@@ -2,24 +2,6 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			vim.lsp.config("pylsp", {
-				settings = {
-					pylsp = {
-						plugins = {
-							pyflakes = { enabled = false },
-							mccabe = { enabled = false },
-							pycodestyle = { enabled = false },
-							autopep8 = { enabled = false },
-							yapf = { enabled = false },
-							pylsp_mypy = { enabled = false },
-							pylsp_black = { enabled = false },
-							pylsp_isort = { enabled = false },
-							flake8 = { enabled = true },
-						},
-					},
-				},
-			})
-
 			vim.lsp.config("ansiblels", {
 				settings = {
 					ansible = {
