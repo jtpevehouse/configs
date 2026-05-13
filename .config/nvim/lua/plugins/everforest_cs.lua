0822048d51de83b2c38a686ae77d643b624357bb
jtpevehouse <josh.pevehouse@gmail.com>
2026-05-13 13:10:25 -0400

    color scheme: move from nord to everforest
    Change-Id: Ifd2638b4d5ba7996adc6ee66042a5e356575ffbc

---
+++ .config/nvim/lua/plugins/everforest_cs.lua
@@ -0,0 +1,39 @@
+return {
+	"neanias/everforest-nvim",
+	priority = 1000,
+	lazy = false,
+	dependencies = { "lukas-reineke/indent-blankline.nvim" },
+	config = function()
+		local everforest = require("everforest")
+		everforest.setup({})
+		everforest.load()
+		local colors = require("everforest.colours")
+		local palette = colors.generate_palette(everforest.config, vim.o.background)
+
+		-- INDENT BLANKLINE
+		local hooks = require("ibl.hooks")
+
+		-- create the highlight groups in the highlight setup hook, so they are reset
+		-- every time the colorscheme changes
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, "green", { fg = palette.green })
+			vim.api.nvim_set_hl(0, "aqua", { fg = palette.aqua })
+			vim.api.nvim_set_hl(0, "blue", { fg = palette.blue })
+			vim.api.nvim_set_hl(0, "orange", { fg = palette.orange })
+			vim.api.nvim_set_hl(0, "purple", { fg = palette.purple })
+		end)
+
+		local highlight = {
+			"green",
+			"aqua",
+			"blue",
+			"orange",
+			"purple",
+		}
+
+		require("ibl").setup({
+			indent = { highlight = highlight },
+			scope = { enabled = false },
+		})
+	end,
+}
