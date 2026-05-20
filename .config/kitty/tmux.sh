46cbfdc672a526555772941d42bb1ebde4dac433
jtpevehouse <josh.pevehouse@gmail.com>
2026-05-20 15:59:07 -0400

    Kitty: fix crash on tmux launch

--- .config/kitty/tmux.sh
+++ .config/kitty/tmux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 TMUX_SESSION_NAME="home"
+WORKSPACE_DIR="/home/jtpevehouse/workspace"
 
-tmux new-session -A -d -s home -n home &&
-	tmux new-window -t $TMUX_SESSION_NAME -n "workspace" -c "~/workspace"
+tmux new-session -A -d -s $TMUX_SESSION_NAME -n "def" &&
+	tmux new-window -t $TMUX_SESSION_NAME -n "workspace" -c "$WORKSPACE_DIR"
 
-tmux attach -t home:def
+tmux attach -t $TMUX_SESSION_NAME:def
