05dc0285c3b06615369da69aec1ed08c9560ce6d
jtpevehouse <josh.pevehouse@gmail.com>
2026-07-06 10:35:58 -0400

    kitty: change launch script back to bash
    Change-Id: Ica2fa64fb18a1c9bc615fceeaf90f740aa9ed72b

--- .config/kitty/tmux.sh
+++ .config/kitty/tmux.sh
@@ -1,10 +1,10 @@
-#!/bin/zsh
+#!/bin/bash
 TMUX_SESSION_NAME="work"
 MRN_DIR="/grmn/prj/mrn"
 
 tmux new-session -A -d -s $TMUX_SESSION_NAME -n def &&
-	tmux new-window -t $TMUX_SESSION_NAME -n "hydrogen" -c "$MRN_DIR/mfd-app" &&
-	tmux new-window -t $TMUX_SESSION_NAME -n "alchemy" -c "$MRN_DIR/alchemy" &&
-	tmux new-window -t $TMUX_SESSION_NAME -n "mrn-metrics" -c "$MRN_DIR/marine-metrics"
+    tmux new-window -t $TMUX_SESSION_NAME -n "hydrogen" -c "$MRN_DIR/mfd-app" &&
+    tmux new-window -t $TMUX_SESSION_NAME -n "alchemy" -c "$MRN_DIR/alchemy" &&
+    tmux new-window -t $TMUX_SESSION_NAME -n "mrn-metrics" -c "$MRN_DIR/marine-metrics"
 
 tmux attach -t $TMUX_SESSION_NAME:def
